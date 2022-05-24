bcc7f77bf2a09584196c57cb9ca1aff2de3db9f4
sein park <seinpark@seins-Mac-mini.local>
2022-05-24 03:00:10 -0400

    Add informational comments for retrofit algo
    Variables and assignments are explained based on the dissertation. It is expected to reveal potential modification directions.

--- R/retrofit.R
+++ R/retrofit.R
@@ -12,15 +12,18 @@
 #' 
 retrofit <- function(x, iterations=4000) {
   if(!is.matrix(x)) stop("x must be a matrix with shape: GeneExpressions:Spots")
+  # the first column is the row names
   rownames(x)=x[,1]
   x=as.matrix(x[,-1])
   
+  # dimensions
   K=8
   G=nrow(x)
   S=ncol(x)
+  L=2*K
   
   ### initialization
-  L=2*K
+  # initial values: alpha_w/beta_w/alpha_th/beta_th/alpha_h/beta_h/
   e=0.05 
   f=0.0001 
   g=0.2 
@@ -28,17 +31,17 @@ retrofit <- function(x, iterations=4000) {
   m=10/K
   n=10
   lamda=0.01
-  
+  # initial matrices
   eta_theta=runif(L,0,1)+m
   gam_theta=runif(L,0,1)+n
   eta_w=matrix(runif(G*L,0,0.5)+e , nrow=G, ncol=L)
   gam_w=matrix(runif(G*L,0,0.005)+f , nrow=G, ncol=L)
   eta_h=matrix(runif(L*S,0,0.1)+g , nrow=L, ncol=S)
   gam_h=matrix(runif(L*S,0,0.5)+h , nrow=L, ncol=S)
-  
+  # phi_alpha, phi_beta
   p=array(rep(0,G*L*S), c(G,L,S))
   si=matrix(0, nrow=G, ncol=L)
-  
+  # W/H/Th from Gamma dist
   Thet=rep(0,L)
   W1=matrix(0,nrow=G, ncol=L)
   H1=matrix(0,nrow=L, ncol=S)
@@ -50,13 +53,15 @@ retrofit <- function(x, iterations=4000) {
   t=1
   
   while(t<iterations){ 
-    rho=(t)^(-kappa) #step size
+    # step (1)
+    rho=(t)^(-kappa)
     
     if(t %% 100 == 0){
       print(paste("Iteration",t))
     }
     
     # simulating from q(.)
+    # step (2)
     for(k in 1:L){
       for(s in 1:S){
         H1[k,s]=rgamma(1, shape=eta_h[k,s], rate=gam_h[k,s])
@@ -64,6 +69,8 @@ retrofit <- function(x, iterations=4000) {
       Thet[k]=rgamma(1, shape=eta_theta[k], rate=gam_theta[k])
       for(v in 1:G){
         W1[v,k]=rgamma(1, shape=eta_w[v,k], rate=gam_w[v,k])
+        
+        # step (3) - phi_beta
         if((W1[v,k]*Thet[k] + lamda)==0){
           si[v,k]=1 ## to avoid numerical error of 0/0 when lamda=0
         } else{
@@ -72,6 +79,7 @@ retrofit <- function(x, iterations=4000) {
       }
     }
     
+    # step (3) - phi_alpha
     for(s in 1:S){
       for(k in 1:L){
         p[,k,s]=((W1[,k] * Thet[k]) +lamda)* H1[k,s]
@@ -81,6 +89,7 @@ retrofit <- function(x, iterations=4000) {
       }
     }
     
+    # for clear assignment
     eta_theta0=eta_theta
     gam_theta0=gam_theta
     eta_h0=eta_h
@@ -88,6 +97,7 @@ retrofit <- function(x, iterations=4000) {
     eta_w0=eta_w
     gam_w0=gam_w
     
+    # step (4) + (5)
     for(k in 1:L){
       eta_w[,k]= (1-rho)*eta_w0[,k] + rho*(e + rowSums(x*p[,k,])*si[,k])
       
@@ -110,7 +120,7 @@ retrofit <- function(x, iterations=4000) {
     }
   }
   
-  
+  # step (6) 
   W_hat=eta_w/gam_w
   H_hat=eta_h/gam_h
   Theta_hat=eta_theta/gam_theta
--- R/simulation.R
+++ R/simulation.R
@@ -1,4 +1,4 @@
-rm(list=ls())
+# rm(list=ls())
 
 dir_path = "~/Research/retrofit/codes"
 in_origin_path = "A3_counts_G=1554.csv"
@@ -8,11 +8,11 @@ out_t_path = "A3_Theta_hat_G=1554.csv"
 
 X=read.csv(paste(dir_path, in_origin_path, sep="/"))
 X = matrix(X)
-#
+
 result = retrofit(x=X, iterations=1)
 
 write.csv(result["h"], paste(dir_path, out_h_path, sep="/"))
 write.csv(result["w"], paste(dir_path, out_w_path, sep="/"))
 write.csv(result["t"], paste(dir_path, out_t_path, sep="/"))
 
-print("finished")
+print("simulation finished")
