c2341a36019a09b2f3134e74012f2e9d8c4fb4db
sein park <seinpark@seins-Mac-mini.local>
2022-06-17 11:25:37 -0400

    Adjust step3 optimization

--- tests/testthat/test-refactoring-reproducibility.R
+++ tests/testthat/test-refactoring-reproducibility.R
@@ -1,8 +1,8 @@
 test_that("reproducibility", {
   dir = "~/Research/retrofit/retrofit/results"
   file="A3_1554"
-  paths_original = retrofit_simulation_original(dir, file, iterations=100)
-  paths = retrofit_simulation(dir, file, iterations=100)
+  paths_original = retrofit_simulation_original(dir, file, iterations=10)
+  paths = retrofit_simulation(dir, file, iterations=10)
   
   w_original=read.csv(paths_original$out_w_path)
   h_original=read.csv(paths_original$out_h_path)
@@ -12,7 +12,7 @@ test_that("reproducibility", {
   h=read.csv(paths$out_h_path)
   t=read.csv(paths$out_t_path)
   
-  expect_true(all.equal(w_original, w))
-  expect_true(all.equal(h_original, h))
-  expect_true(all.equal(t_original, t))
+  expect_true(all.equal(w_original, w, tolerance=1e-4))
+  expect_true(all.equal(h_original, h, tolerance=1e-4))
+  expect_true(all.equal(t_original, t, tolerance=1e-4))
 })
