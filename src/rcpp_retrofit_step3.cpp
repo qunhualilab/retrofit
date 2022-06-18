e7cb3d0eb1c6e449067172e6910f4babe6170ea1
sein park <seinpark@seins-Mac-mini.local>
2022-06-18 07:20:24 -0400

    Improve step3 performance slightly

--- src/rcpp_retrofit_step3.cpp
+++ src/rcpp_retrofit_step3.cpp
@@ -25,24 +25,50 @@ NumericVector retrofit_step3_alpha_numerator(NumericVector W_gk, NumericVector T
   NumericVector::iterator W_gk_iter = W_gk.begin();
   NumericVector::iterator H_ks_iter = H_ks.begin();
   NumericVector::iterator TH_k_iter = TH_k.begin();
+  // for(int s=0; s<S; ++s){
+  //   for(int k=0; k<K; ++k){
+  //     for(int g=0; g<G; ++g){
+  //       *phi_a_gks_iter = ((*W_gk_iter)*(*TH_k_iter) + lambda)*(*H_ks_iter);
+  //       ++phi_a_gks_iter;
+  // 
+  //       // equivalent: W_gk[k*G+g];
+  //       ++W_gk_iter; if(g==G-1 && k==K-1) {W_gk_iter -= K*G;}
+  // 
+  //       // equivalent: H_ks[s*K+k];
+  //       if(g==G-1){++H_ks_iter;}
+  // 
+  //       // equivalent: TH_k[k];
+  //       if(g==G-1) {++TH_k_iter;} if(g==G-1 && k==K-1) {TH_k_iter -= K;}
+  //     }
+  //   }
+  // }
+
+  NumericVector mul_gk (G*K);
+  NumericVector::iterator mul_gk_iter = mul_gk.begin();
+  for(int k=0; k<K; ++k){
+    for(int g=0; g<G; ++g){
+      *mul_gk_iter = (*W_gk_iter)*(*TH_k_iter) + lambda;
+      ++mul_gk_iter;
+      ++W_gk_iter;
+    }
+    ++TH_k_iter;
+  }
+  mul_gk_iter = mul_gk.begin();
+  float H_ks_val;
   for(int s=0; s<S; ++s){
     for(int k=0; k<K; ++k){
+      H_ks_val = *H_ks_iter;
+      ++H_ks_iter;
+  
       for(int g=0; g<G; ++g){
-        *phi_a_gks_iter = ((*W_gk_iter)*(*TH_k_iter) + lambda)*(*H_ks_iter);
+        *phi_a_gks_iter = (*mul_gk_iter)*H_ks_val;
         ++phi_a_gks_iter;
-        
+  
         // equivalent: W_gk[k*G+g];
-        ++W_gk_iter; if(g==G-1 && k==K-1) {W_gk_iter -= K*G;}
-        
-        // equivalent: H_ks[s*K+k];
-        if(g==G-1){++H_ks_iter;}
-        
-        // equivalent: TH_k[k];
-        if(g==G-1) {++TH_k_iter;} if(g==G-1 && k==K-1) {TH_k_iter -= K;}
+        ++mul_gk_iter; if(g==G-1 && k==K-1) {mul_gk_iter -= K*G;}
       }
     }
   }
-
   return phi_a_gks;
 }
 
@@ -121,20 +147,29 @@ NumericVector retrofit_step3_alpha(NumericVector W_gk, NumericVector TH_k, Numer
   NumericVector::iterator W_gk_iter = W_gk.begin();
   NumericVector::iterator H_ks_iter = H_ks.begin();
   NumericVector::iterator TH_k_iter = TH_k.begin();
+  NumericVector mul_gk (G*K);
+  NumericVector::iterator mul_gk_iter = mul_gk.begin();
+  for(int k=0; k<K; ++k){
+    for(int g=0; g<G; ++g){
+      *mul_gk_iter = (*W_gk_iter)*(*TH_k_iter) + lambda;
+      ++mul_gk_iter;
+      ++W_gk_iter;
+    }
+    ++TH_k_iter;
+  }
+  mul_gk_iter = mul_gk.begin();
+  float H_ks_val;
   for(int s=0; s<S; ++s){
     for(int k=0; k<K; ++k){
+      H_ks_val = *H_ks_iter;
+      ++H_ks_iter;
+      
       for(int g=0; g<G; ++g){
-        *phi_a_gks_iter = ((*W_gk_iter)*(*TH_k_iter) + lambda)*(*H_ks_iter);
+        *phi_a_gks_iter = (*mul_gk_iter)*H_ks_val;
         ++phi_a_gks_iter;
         
         // equivalent: W_gk[k*G+g];
-        ++W_gk_iter; if(g==G-1 && k==K-1) {W_gk_iter -= K*G;}
-        
-        // equivalent: H_ks[s*K+k];
-        if(g==G-1){++H_ks_iter;}
-        
-        // equivalent: TH_k[k];
-        if(g==G-1) {++TH_k_iter;} if(g==G-1 && k==K-1) {TH_k_iter -= K;}
+        ++mul_gk_iter; if(g==G-1 && k==K-1) {mul_gk_iter -= K*G;}
       }
     }
   }
