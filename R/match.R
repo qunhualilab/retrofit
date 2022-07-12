5589150458cf4a21f7e1d1d1e80a5409852d35bd
sein park <seinpark@seins-Mac-mini.local>
2022-07-12 15:38:06 -0400

    Fix matching algorithm to index columns with provided parameter
    Former-commit-id: 89b56403d672bcd060d2846df869bcf79cd2da74

--- R/match.R
+++ R/match.R
@@ -29,7 +29,7 @@ RetrofitMatch <- function(ref_w, decomp_w, decomp_h, K) {
   
   correlations = cor(ref_w, w)
   correlations2 = cor(ref_w, w)
-  print(correlations)
+  
   col_sel=rep(NA,K)
   row_sel=rep(NA,K)
   for(i in 1:K){
@@ -38,15 +38,15 @@ RetrofitMatch <- function(ref_w, decomp_w, decomp_h, K) {
     r1=which(correlations2 == max(correlations2), arr.ind=TRUE)[1]
     c1=which(correlations2 == max(correlations2), arr.ind=TRUE)[2]
     
-    print(paste("r2:", r2, cell_types[r2], "r1:", r1, cell_types[r1]))
-    
     row_sel[i]=r2
-    col_sel[r2]=c2
+    col_sel[i]=c2
     
     if(i<K){
       correlations2=correlations2[-r1,-c1]
     }
   }
+  
+  print(col_sel)
   
   cell_mod = rep(NA, K)
   for (i in 1:K) {
@@ -55,6 +55,8 @@ RetrofitMatch <- function(ref_w, decomp_w, decomp_h, K) {
   
   w_mod = w[,col_sel]
   h_mod = h[col_sel,]
+  
+  print(dim(w_mod))
   colnames(w_mod) = cell_mod
   rownames(h_mod) = cell_mod
   
