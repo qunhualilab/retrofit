708d2156abea6613e92bb34f230bd4815f9144ff
Adam Park <akp6031@psu.edu>
2022-09-02 09:21:01 -0400

    Add main simulation
    Former-commit-id: d163258bf94a8c35e9398e3123d11e21ea5a48d5

---
+++ tests/testthat/test-main-simulation.R
@@ -0,0 +1,63 @@
+# test_that("reproducibility", {
+#   iterations = 10
+#   file_source = "sample_x"
+#   ref_file_source = "sample_ref_cor"
+#   ref_marker_file_source = "sample_ref_marker"
+#   L = 16
+#   K = 8
+#   seed = 1
+#   
+#   in_dir = "../../data"
+#   result_dir = "../../data/sample_results"
+#   ref_w_path = paste(in_dir, paste(ref_file_source, ".csv", sep=""), sep="/")
+#   ref_w=read.csv(ref_w_path, row.names = 1, check.names = FALSE)
+#   ref_marker_path = paste(in_dir, paste(ref_marker_file_source, ".csv", sep=""), sep="/")
+#   ref_marker_d=read.csv(ref_marker_path, check.names = FALSE)
+#   ref_marker = list()
+#   for(r in 1:nrow(ref_marker_d)){
+#     gene = ref_marker_d[[1]][r]
+#     cell_type = ref_marker_d[[2]][r]
+#     if(is.null(ref_marker[[cell_type]])){
+#       ref_marker[[cell_type]] = c()
+#     }
+#     ref_marker[[cell_type]] = c(ref_marker[[cell_type]], gene)
+#   }
+#   
+#   in_path = paste(in_dir, paste(file_source, ".csv", sep=""), sep="/")
+#   x=read.csv(in_path, row.names = 1, check.names = FALSE)
+#   
+#   result = RetrofitMain(x, 
+#                         ref_cor=ref_w, 
+#                         ref_marker=ref_marker, 
+#                         iterations=iterations, 
+#                         L=L, 
+#                         K=K,
+#                         seed=seed)
+#   
+#   decomp_h_path       = paste(result_dir, paste(file_source,"__decomp_h.csv", sep=""), sep="/")
+#   decomp_w_path       = paste(result_dir, paste(file_source,"__decomp_w.csv", sep=""), sep="/")
+#   decomp_th_path      = paste(result_dir, paste(file_source,"__decomp_th.csv", sep=""), sep="/")
+#   match_cor_w_path    = paste(result_dir, paste(file_source,"__map_cor_w.csv", sep=""), sep="/")
+#   match_cor_h_path    = paste(result_dir, paste(file_source,"__map_cor_h.csv", sep=""), sep="/")
+#   match_marker_w_path = paste(result_dir, paste(file_source,"__map_marker_w.csv", sep=""), sep="/")
+#   match_marker_h_path = paste(result_dir, paste(file_source,"__map_marker_h.csv", sep=""), sep="/")
+#   
+#   decomp_h        = read.csv(decomp_h_path, row.names = 1, check.names = FALSE)
+#   decomp_w        = read.csv(decomp_w_path, row.names = 1, check.names = FALSE)
+#   decomp_th       = read.csv(decomp_th_path, row.names = 1, check.names = FALSE)
+#   match_cor_h     = read.csv(match_cor_h_path, row.names = 1, check.names = FALSE)
+#   match_cor_w     = read.csv(match_cor_w_path, row.names = 1, check.names = FALSE)
+#   match_marker_h  = read.csv(match_marker_h_path, row.names = 1, check.names = FALSE)
+#   match_marker_w  = read.csv(match_marker_w_path, row.names = 1, check.names = FALSE)
+#   
+#   expect_true(all.equal(result$w, decomp_w))
+#   expect_true(all.equal(result$h, decomp_h))
+#   expect_true(all.equal(result$th, decomp_th))
+#   expect_true(all.equal(result$w_cor_map, match_cor_w))
+#   expect_true(all.equal(result$h_cor_map, match_cor_h))
+#   expect_true(all.equal(result$w_marker_map, match_marker_w))
+#   expect_true(all.equal(result$h_marker_map, match_marker_h))
+# })
+# 
+# 
+
