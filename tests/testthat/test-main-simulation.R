990a16c0398097daf3c887c5d1396159fac76980
sein park <seinpark@seins-MacBook-Pro.local>
2022-12-28 05:20:28 -0500

    Finalize main simulation
    Former-commit-id: 1d57cab4a0af3af7c1de9f7bcf39e5343ccd8af7
    Former-commit-id: 852bd38c530faf1f931cdea9fb67618eca48ecc1

--- tests/testthat/test-main-simulation.R
+++ tests/testthat/test-main-simulation.R
@@ -1,62 +1,30 @@
 test_that("main-simulation-works", {
   iterations = 10
-  file_source = "sample_x"
-  ref_file_source = "sc_ref"
-  ref_marker_file_source = "marker_ref"
   L = 16
   K = 8
   seed = 1
   
-  in_dir = "../../data"
-  result_dir = "../../data/sample_results"
-  ref_w_path = paste(in_dir, paste(ref_file_source, ".csv", sep=""), sep="/")
-  ref_w=read.csv(ref_w_path, row.names = 1, check.names = FALSE)
-  ref_marker_path = paste(in_dir, paste(ref_marker_file_source, ".csv", sep=""), sep="/")
-  ref_marker_d=read.csv(ref_marker_path, check.names = FALSE)
-  ref_marker = list()
-  for(r in 1:nrow(ref_marker_d)){
-    gene = ref_marker_d[[1]][r]
-    cell_type = ref_marker_d[[2]][r]
-    if(is.null(ref_marker[[cell_type]])){
-      ref_marker[[cell_type]] = c()
-    }
-    ref_marker[[cell_type]] = c(ref_marker[[cell_type]], gene)
-  }
-  
-  in_path = paste(in_dir, paste(file_source, ".csv", sep=""), sep="/")
-  x=read.csv(in_path, row.names = 1, check.names = FALSE)
-  
+  data(test_main_simulation)
+  x           = test_main_simulation$n10m3_x
+  sc_ref      = test_main_simulation$sc_ref
+  marker_ref  = test_main_simulation$marker_ref
+  results     = test_main_simulation$results
+
   result = Retrofit(x, 
-                    ref_cor=ref_w, 
-                    ref_marker=ref_marker, 
-                    iterations=iterations, 
-                    L=L, 
-                    K=K,
-                    seed=seed)
+                ref_cor=sc_ref, 
+                ref_marker=marker_ref, 
+                iterations=iterations, 
+                L=L, 
+                K=K,
+                seed=seed)
   
-  decomp_h_path       = paste(result_dir, paste(file_source,"__decomp_h.csv", sep=""), sep="/")
-  decomp_w_path       = paste(result_dir, paste(file_source,"__decomp_w.csv", sep=""), sep="/")
-  decomp_th_path      = paste(result_dir, paste(file_source,"__decomp_th.csv", sep=""), sep="/")
-  match_cor_w_path    = paste(result_dir, paste(file_source,"__map_cor_w.csv", sep=""), sep="/")
-  match_cor_h_path    = paste(result_dir, paste(file_source,"__map_cor_h.csv", sep=""), sep="/")
-  match_marker_w_path = paste(result_dir, paste(file_source,"__map_marker_w.csv", sep=""), sep="/")
-  match_marker_h_path = paste(result_dir, paste(file_source,"__map_marker_h.csv", sep=""), sep="/")
-  
-  decomp_h        = read.csv(decomp_h_path, row.names = 1, check.names = FALSE)
-  decomp_w        = read.csv(decomp_w_path, row.names = 1, check.names = FALSE)
-  decomp_th       = read.csv(decomp_th_path, row.names = 1, check.names = FALSE)
-  match_cor_h     = read.csv(match_cor_h_path, row.names = 1, check.names = FALSE)
-  match_cor_w     = read.csv(match_cor_w_path, row.names = 1, check.names = FALSE)
-  match_marker_h  = read.csv(match_marker_h_path, row.names = 1, check.names = FALSE)
-  match_marker_w  = read.csv(match_marker_w_path, row.names = 1, check.names = FALSE)
-  
-  expect_true(all.equal(matrix(unlist(decomp_h)), result$h, check.attributes = FALSE))
-  expect_true(all.equal(matrix(unlist(decomp_w)), result$w, check.attributes = FALSE))
-  expect_true(all.equal(matrix(unlist(decomp_th)), result$t, check.attributes = FALSE))
-  expect_true(all.equal(matrix(unlist(match_cor_h)), result$h_cor_map, check.attributes = FALSE))
-  expect_true(all.equal(matrix(unlist(match_cor_w)), result$w_cor_map, check.attributes = FALSE))
-  expect_true(all.equal(matrix(unlist(match_marker_h)), result$h_marker_map, check.attributes = FALSE))
-  expect_true(all.equal(matrix(unlist(match_marker_w)), result$w_marker_map, check.attributes = FALSE))
+  expect_true(all.equal(as.matrix(results$decomp_h), result$h, check.attributes = FALSE))
+  expect_true(all.equal(as.matrix(results$decomp_w), result$w, check.attributes = FALSE))
+  expect_true(all.equal(as.matrix(results$decomp_th), result$t, check.attributes = FALSE))
+  expect_true(all.equal(as.matrix(results$match_cor_h), result$h_cor_map, check.attributes = FALSE))
+  expect_true(all.equal(as.matrix(results$match_cor_w), result$w_cor_map, check.attributes = FALSE))
+  expect_true(all.equal(as.matrix(results$match_marker_h), result$h_marker_map, check.attributes = FALSE))
+  expect_true(all.equal(as.matrix(results$match_marker_w), result$w_marker_map, check.attributes = FALSE))
 })
 
 
