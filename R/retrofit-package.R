32b53945d3b7f5fce07b8b066e8c6f67d24d8661
Adam Park <akp6031@psu.edu>
2023-01-02 23:47:38 -0500

    [BiocCheck] resolve expose rcpp warning
    Former-commit-id: f303b73686a0a0ee0ed1a2356057371597c09404
    Former-commit-id: 7a04150b798d46d9be128e959680992794e01457 [formerly 961362a214a212383eb45edb881a33416647f1a6]
    Former-commit-id: fa94db166bd681ebd9905ac1e47d9b1b0f9a9ef4

--- R/retrofit-package.R
+++ R/retrofit-package.R
@@ -1,4 +1,8 @@
 ## usethis namespace: start
+#' @docType package
+#' @import Rcpp
+#' @importFrom Rcpp evalCpp
 #' @useDynLib retrofit, .registration = TRUE
+#' @name retrofit
 ## usethis namespace: end
 NULL
