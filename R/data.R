e24c98a3e5589d0babdca5e3942f7f96b47043e3
Adam Park <akp6031@psu.edu>
2023-02-11 23:55:28 -0500

    Remove data files and get more space

--- R/data.R
+++ R/data.R
@@ -26,72 +26,16 @@
 #' @format Includes colon scenario x, references, a large iterations results.
 "vignetteColonData"
 
-#' retrofit test
+#' simulation data
 #'
-#' A dataset with input and output of "retrofit" for reproducibility tests.
+#' A dataset with input and output of retrofit functions for reproducibility tests.
 #'
 #' \itemize{
-#'   \item testRetrofitData
+#'   \item testSimulationData
 #' }
 #'
 #' @docType data
-#' @name testRetrofitData
-#' @usage data(testRetrofitData)
+#' @name testSimulationData
+#' @usage data(testSimulationData)
 #' @format Includes input x, references and results with large iterations
-"testRetrofitData"
-
-#' decompose test
-#'
-#' A dataset with input and output of "decompose" for reproducibility tests.
-#'
-#' \itemize{
-#'   \item testDecomposeData
-#' }
-#'
-#' @docType data
-#' @name testDecomposeData
-#' @usage data(testDecomposeData)
-#' @format Includes input x and output w, h, and th
-"testDecomposeData"
-
-#' Readme
-#'
-#' A dataset supporting readme example
-#'
-#' \itemize{
-#'   \item readmeData
-#' }
-#'
-#' @docType data
-#' @name readmeData
-#' @usage data(readmeData)
-#' @format Includes input x
-"readmeData"
-
-#' annotate with correlation test
-#'
-#' A dataset with input&output of "annotateWithCorrelations"
-#'
-#' \itemize{
-#'   \item testAnnotateWithCorrelationsData
-#' }
-#'
-#' @docType data
-#' @name testAnnotateWithCorrelationsData
-#' @usage data(testAnnotateWithCorrelationsData)
-#' @format Includes input decomp_w, decomp_h, sc_ref and output cell types
-"testAnnotateWithCorrelationsData"
-
-#' annotate with markers test
-#'
-#' A dataset with input&output of "annotateWithMarkers" reproducibility
-#'
-#' \itemize{
-#'   \item testAnnotateWithMarkersData
-#' }
-#'
-#' @docType data
-#' @name testAnnotateWithMarkersData
-#' @usage data(testAnnotateWithMarkersData)
-#' @format Includes input decomp_w, decomp_h, sc_ref and output cell types
-"testAnnotateWithMarkersData"
+"testSimulationData"
